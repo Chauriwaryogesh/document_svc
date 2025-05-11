c3d10227b50b6103775f94c890bccaa3bb571062
Yogesh <chauriwaryogesh@gmail.com>
2025-05-11 22:57:33 +0530

    removing extra packages

--- src/main/java/com/secureportal/repo/IWorkflowService.java
+++
@@ -1,9 +0,0 @@
-package com.example.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.WorkFlowService;
-
-public interface IWorkflowService extends JpaRepository<WorkFlowService, String> {
-
-}
--- src/main/java/com/secureportal/service/IWorkItemService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.dto.WorkItemDTO;
-
-public interface IWorkItemService {
-
-	WorkItemDTO createWorkItem(WorkItemDTO workItemRequest, String userId);
-
-}
