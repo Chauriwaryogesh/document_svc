ad52c4daa4893101a7ba0fa6061d5ddd45b8a6c6
Yogesh <chauriwaryogesh@gmail.com>
2025-09-07 21:54:56 +0530

    ok

--- src/main/java/com/SecureAccessPortal/Service/PaymentService.java
+++ src/main/java/com/SecureAccessPortal/Service/PaymentService.java
@@ -1,6 +1,5 @@
 package com.SecureAccessPortal.Service;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,10 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.scheduling.annotation.Async;
